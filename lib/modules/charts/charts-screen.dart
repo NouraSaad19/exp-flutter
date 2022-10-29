be9b704b2ce94551ae47639f8373d8be29bc7e5e
Muhannad Saeed Almutairi <muhnndsaeed@Muhannads-MacBook-Pro.local>
2022-10-29 18:53:56 +0300

    a

--- lib/modules/charts/charts-screen.dart
+++ lib/modules/charts/charts-screen.dart
@@ -1,12 +1,59 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class ChartScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:expense_tracker/ui/component/expensesItem.dart';
+import 'package:expense_tracker/ui/component/expensesItem.dart';
+import 'dart:core';
+
+var a2 = addtypeExpenses.map((e) => e) as String;
+var a1 = addamountExpenses.map((e) => e) as double;
+
+// var a2 = addtypeExpenses.reduce((value, element) => value + element);
+
+class ChartScreen extends StatefulWidget {
   const ChartScreen({super.key});
+
+  @override
+  State<ChartScreen> createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> {
+  late List<GDPData> _chartData;
+  @override
+  void initState() {
+    _chartData = getChartData(GDPData(a2, a1));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("ChartPage"),
+      child: SfCircularChart(
+        title: ChartTitle(text: "Expence"),
+        legend:
+            Legend(isVisible: true, overflowMode: LegendItemOverflowMode.wrap),
+        series: <CircularSeries>[
+          DoughnutSeries<GDPData, String>(
+            dataSource: _chartData,
+            xValueMapper: (GDPData data, _) => data.continent,
+            yValueMapper: (GDPData data, _) => data.gdp,
+            dataLabelSettings: DataLabelSettings(
+                isVisible: true, labelPosition: ChartDataLabelPosition.outside),
+          ),
+        ],
+      ),
     );
   }
+}
+
+List<GDPData> getChartData(GDPData gdpData) {
+  List<GDPData> chartData = [GDPData(a2, a1)];
+  return chartData;
+}
+
+class GDPData {
+  GDPData(this.continent, this.gdp);
+  final String continent;
+  final double gdp;
 }
